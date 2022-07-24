f9e460056950c57d9aedaa256271a56b0291d521
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-07-24 20:11:02 +0300

    header, main and footer files has been created

---
+++ demo/src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class H1header extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>First react app</h1>
+      </>
+    );
+  }
+}
+
+export default H1header;

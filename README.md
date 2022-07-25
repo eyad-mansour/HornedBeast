b925ded9c5338a299c15297275b206bf0c9c7c80
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-07-25 23:09:37 +0300

    didnt finish

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ cards in react bootstrap they have img or footer and header etc.
 on netlify we need to add to deploy our page CI= npm run...
 
 i solved the problem regarding me not sending src and alt for the imgaes
+
+Number and name of feature: i know how to install nav form react and i know howw to put card on my website but it didnt pass the data
+
+Estimate of time needed to complete: didnt finish
+
+Start time: 6pm
+
+Finish time: 11:30
+
+Actual time needed to complete: didnt complete
--- src/component/HornedBeastFi.js
+++ src/component/HornedBeastFi.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class HorendBeastC extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
   render() {
     return (
       <>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.title} />
+        <img
+          src={this.props.imageUrl}
+          alt={this.props.title}
+          onClick={this.handleClick}
+        />
         <p>{this.props.description}</p>
+        <span>👍 like{this.state.counter}</span>
       </>
     );
   }

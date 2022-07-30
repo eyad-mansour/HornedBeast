9a6f4a221abe56c69bec86418ce8e1e3be187752
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-07-30 20:09:31 +0300

    sorted card

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import "./App.css";
 import "./component/Header";
 import FooterRights from "./component/footer";
 import H1header from "./component/Header";
+import SelectedBeast from "./component/SelectedBeast";
 import Main from "./component/main";
-import Form from "./component/Form";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <H1header />
 
-        <Form />
-        <Main />
-
-        <FooterRights />
-      </header>
-    </div>
-  );
+import React from "react";
+import data from "./component/data.json";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <H1header />
+          <Main />
+          <FooterRights />
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
